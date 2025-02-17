c2ce7826485f78e38a7a552db2961df422a2a3d2
Dadiya-Harsh-wappnet <harshd.wappnet@gmail.com>
2025-02-17 23:06:43 +0530

    Removed uneccesary file from tracking..

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
-    PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
-    PINECONE_INDEX_NAME = 'cure-now'
